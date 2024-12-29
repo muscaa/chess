19151f0aa95c29a2ec2493679b3c303f297e41e0
Musca Mihail <muscamihaill@gmail.com>
2024-12-29 23:22:38 +0200

    fixed pawn moving 2 steps when blocked

--- shared/src/main/java/muscaa/chess/board/piece/pieces/PawnPiece.java
+++ shared/src/main/java/muscaa/chess/board/piece/pieces/PawnPiece.java
@@ -21,14 +21,13 @@ public class PawnPiece extends AbstractPiece {
 	public void findMoves(Map<Cell, AbstractMove> moves, Matrix matrix, Cell from) {
 		Cell direction = team == TeamRegistry.WHITE ? new Cell(0, -1) : new Cell(0, 1);
 		
-		if (totalMoves == 0) {
-			// TODO check if first step is blocked
+		Cell oneStep = from.add(direction);
+		PieceUtils.basicPromote(this, moves, matrix, oneStep);
+		
+		if (totalMoves == 0 && matrix.isInBounds(oneStep) && matrix.get(oneStep) == NullPiece.INSTANCE) {
 			Cell twoSteps = from.add(direction.multiply(2));
 			PieceUtils.basicPromote(this, moves, matrix, twoSteps);
 		}
-		
-		Cell oneStep = from.add(direction);
-		PieceUtils.basicPromote(this, moves, matrix, oneStep);
 		
 		Cell left = from.add(direction).add(Cell.ONE_ZERO.negate());
 		PieceUtils.capturePromote(this, moves, matrix, left);
