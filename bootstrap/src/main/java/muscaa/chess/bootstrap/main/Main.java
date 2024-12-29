f40578611e698a9882d460e5755d931882878296
Musca Mihail <muscamihaill@gmail.com>
2024-12-29 22:28:10 +0200

    restructure project from 3 bootstraps to 1

--- bootstrap/src/main/java/muscaa/chess/bootstrap/main/Main.java
+++ bootstrap/src/main/java/muscaa/chess/bootstrap/main/Main.java
@@ -6,7 +6,12 @@ import muscaa.chess.bootstrap.DefaultBootstrap;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		Bootstrap.INSTANCE = new DefaultBootstrap("muscaa.chess.client.lwjgl.Lwjgl3Launcher");
+		String mainClass = System.getProperty("main.class");
+		if (mainClass == null || mainClass.isBlank()) {
+			throw new IllegalStateException("Property main.class not set!");
+		}
+		
+		Bootstrap.INSTANCE = new DefaultBootstrap(mainClass);
 		Bootstrap.INSTANCE.launch(args);
 	}
 }
